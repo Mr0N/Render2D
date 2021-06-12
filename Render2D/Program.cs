0f71dc538aae24c9450c7554962396b85877ebb8
new_user <new_user@gmail.com>
2021-06-12 15:31:12 +0300

    Добавьте файлы проекта.

---
+++ Render2D/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using Svg.Skia;
+
+namespace Render
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var svg = new SKSvg())
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                svg.Load("image.svg");
+                stopWatch.Stop();
+                Console.WriteLine("Parsing:{0}", stopWatch.ElapsedMilliseconds);
+                stopWatch.Reset();
+                stopWatch.Start();
+                svg.Save("image.png", SKColor.Empty, SKEncodedImageFormat.Png, 100, 10f, 10f);
+                stopWatch.Stop();
+                Console.WriteLine("Render:{0}", stopWatch.ElapsedMilliseconds);
+            }
+        }
+
+    }
+}
